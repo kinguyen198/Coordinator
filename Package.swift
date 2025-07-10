523497040eabd617f446414e34b46908968518f0
kinguyen198 <67310086+kinguyen198@users.noreply.github.com>
2025-07-11 02:50:31 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 let package = Package(
     name: "Coordinator",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v13),
-        .tvOS(.v13),
+        .iOS(.v14),
+        .macOS(.v14),
+        .tvOS(.v14),
         .watchOS(.v6)
     ],
     products: [
